8bdf3f4b338eaa4ec5b6970d8e7adc030d2630e2
Gokul prabhu <gokul.prabhu@freshworks.com>
2018-06-19 18:19:09 +0530

    check point

--- server/config/routes.rb
+++ server/config/routes.rb
@@ -2,4 +2,5 @@ Rails.application.routes.draw do
   resources :tasks
   resources :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  match '/signup', to: 'users#new', via: 'get'
 end
