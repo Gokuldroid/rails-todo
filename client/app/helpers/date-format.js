d93e3049f05278c2e2f65467994767dce723e701
Gokul prabhu <gokul.prabhu@freshworks.com>
2018-06-26 18:40:09 +0530

    Date format fix

--- client/app/helpers/date-format.js
+++ client/app/helpers/date-format.js
@@ -1,7 +1,7 @@
 import { helper } from '@ember/component/helper';
 
 export function dateFormat(params) {
-  return params && params[0] && moment(params[0]).format('MMMM Do YYYY, h:mm:ss a');
+  return params && params[0] && moment(params[0]).format('MMMM Do YYYY, h:mm a');
 }
 
 export default helper(dateFormat);
